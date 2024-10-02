dac26c39af935d00a51f870bc48e0b608126c9f8
BALLAMOUSSAKEITA <ballamoussa.keita@esprit.tn>
2024-10-02 10:35:38 +0100

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 
-
+//Test ajout commentaires
 	private static final long serialVersionUID = 1L;
 
 	@Id
